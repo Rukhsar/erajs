ea68312bd23ce4a1a9f7e81c6e8064b0fdfb222b
Rukhsar Manzoor <rukhsar.man@gmail.com>
2016-05-17 09:25:00 +0100

    AMD vs CommonJS vs window.global exposure

--- era.js
+++ era.js
@@ -6,3 +6,29 @@
  * @url     www.rukhsar.me.uk
  * @version 1.0
  */
+
+(function (root,factory) {
+
+    if (typeof exports === 'object') {
+
+        // Common JavaScript
+        module.exports = factory(root, root.document);
+
+    } else if ( typeof define === 'function' && define.amd ) {
+
+        // AMD
+
+        define(function() { return factory(root, root.document); });
+
+    } else {
+
+    root.fromNow = factory(root, root.document); // window global
+
+}
+
+    }(typeof window !== 'undefined' ? window : this, function (window, document) {
+
+  'use strict';
+
+
+});
